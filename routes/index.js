eab08c24447bdc13617c6ce2730e37bd442852ea
Wassim.rb <rbilawassim@gmail.coml>
2024-04-24 15:42:02 +0200

    first commit moviz5 backend

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = router;
+
+
+router.get('/movies', (req, res) => {
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.OWM_API_KEY,
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(movies => res.json({movies:movies.results}))
+      .catch(err => console.error('error:' + err));
+
+});
