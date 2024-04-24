0d5ff505cd4c189cd5888fe90462e964fa93044a
Wassim.rb <rbilawassim@gmail.coml>
2024-04-24 16:48:45 +0200

    update for vercel

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://mymoviz-part5-backend-cyan-chi.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://mymoviz-part5-frontend-zeta.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
